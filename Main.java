5ec6a44d3597a45e96c5063892ab8d91af11b91d
Unknown <yefer01091@hotmail.com>
2018-02-13 09:28:54 -0500

    Primera Clase
    Clase con las operaciones básicas: suma, resta multiplicacion y división.

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+	public static void main(String[] arg){
+		System.out.println("");
+		System.out.println("Clase de programacion");
+		System.out.println("");
+		System.out.println("\n\n\t Ejercicio N°1");
+		System.out.println("");
+		System.out.println("");
+
+		float var1 = 20;
+		float var2 = 50;
+		
+		System.out.println("La suma de la variable var1 + var2 = "+(var1+var2));
+		System.out.println("La resta de la variable var1 - var2 = "+(var1-var2));
+		System.out.println("La multiplicacion de la variable var1 * var2 = "+(var1*var2));
+		System.out.println("La division de la variable var1 / var2 = "+(var1/var2));
+	}
+}
